10c1d0fe83f3a5783b543b32ddbb341155c18410
lohargaurav00 <lohargaurav00@gmail.com>
2023-08-17 13:49:28 +0530

    added .env in git ignore update .env.example

--- utils/blogPost.ts
+++ utils/blogPost.ts
@@ -4,12 +4,6 @@ import { createClient } from "contentful";
 const spaceId = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || "";
 const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || "" ;
 
-export const test = () => {
-  console.log(spaceId)
-  console.log(accessToken)
-}
-
-
 const client = createClient({
   space: spaceId,
   accessToken: accessToken,
